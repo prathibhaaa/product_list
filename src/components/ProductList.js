33f4bc5ca390d1b27a546d89e38db7f4e1765b96
prathibha <prathibhaprasad88@gmail.com>
2020-07-08 17:11:34 +0400

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,12 +3,9 @@ import React, { Component } from 'react'
 export default class ProductList extends Component {
 
 
-// constructor(props) {
-//     super(props);
-//     const products = this.props.products
-//   }
 
   render() {
+    const {id, name, currency, department, price } = this.props.columns
     return (
       <div id="product-list">
         <header>
@@ -17,23 +14,68 @@ export default class ProductList extends Component {
         <table>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Department</th>
-              <th>Price</th>
+               {id && <th>ID</th>}
+               {name && <th>Name</th>}
+               {currency && <th>Currency</th>}
+               {department && <th>Department</th>}
+               {price && <th>Price</th>}
             </tr>
           </thead>
           <tbody>
-              {this.props.products.map((prd, id) =>
-                <tr key={prd.id}>
-                <td>{prd.id}</td>
-                <td>{prd.name}</td>
-                <td>{prd.department}</td>
-                <td>{prd.price}</td>
-              </tr>)}
+              {this.props.products.map((prd, index) =>
+                <tr key={index}>
+                 {id && <td>{prd.id}</td>}
+                 {name && <td>{prd.name}</td>}
+                 {currency && <td>{prd.currency}</td>}
+                 {department && <td>{prd.department}</td>}
+                 {price && <td>{prd.price}</td>}
+              </tr>
+              )}
           </tbody>
         </table>
       </div>
     )
   }
 }
+
+
+// import React, { Component } from 'react'
+
+// export default class ProductList extends Component {
+
+// render() {
+//   const columns =this.props.columns
+//     return (
+//       <div id="product-list">
+//         <header>
+//           <strong>Product List</strong>
+//         </header>
+//         <table>
+//           <thead>
+//             <tr>
+//               for(let column in columns) {
+//                 if(columns[column]) {
+//                  `<td>{this.props.products[column]}</td>`
+//                 }
+//               }
+//               <th>ID</th>
+//               <th>Name</th>
+//               <th>Department</th>
+//               <th>Price</th>
+//             </tr>
+
+//           </thead>
+//           <tbody>
+//               {this.props.products.map((prd, id) =>
+//                 <tr key={id}>
+//                 <td>{prd.id}</td>
+//                 <td>{prd.name}</td>
+//                 <td>{prd.department}</td>
+//                 <td>{prd.price}</td>
+//               </tr>)}
+//           </tbody>
+//         </table>
+//       </div>
+//     )
+//   }
+// }
